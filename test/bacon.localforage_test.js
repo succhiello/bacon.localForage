d839e56384856538a03323c4eb5531804c7e1dcc
kiri <satoshi_ebihara@xica.net>
2014-11-16 22:35:33 +0900

    add localforage config API aliases and dist.

--- test/bacon.localforage_test.js
+++ test/bacon.localforage_test.js
@@ -1,11 +1,6 @@
 describe('bacon.localforage', function() {
 
-    localforage.config({
-        driver: localforage.LOCALSTORAGE,
-        name: 'bacon.localforage test'
-    });
-
-    var blc = require('../src/bacon.localforage'),
+    var blf = require('../src/bacon.localforage'),
         data = {
             key0: 'Alpha',
             key1: 'Bravo',
@@ -13,10 +8,23 @@ describe('bacon.localforage', function() {
             key3: 'Delta'
         };
 
+    blf.config({
+        driver: blf.LOCALSTORAGE,
+        name: 'bacon.localforage test'
+    });
+
+    it('should clear storage', function(done) {
+        blf.clear()
+            .flatMap(blf.length())
+            .map(expect)
+            .doAction('.toBe', 0)
+            .assign(done);
+    });
+
     it('should set and get item and return Bacon.js EventStream', function(done) {
-        Bacon.zipAsArray(_.map(data, function(v, k) { return blc.setItem(k, v); }))
+        Bacon.zipAsArray(_.map(data, function(v, k) { return blf.setItem(k, v); }))
             .doAction(function(values) {
-                Bacon.zipAsArray(_(data).keys().map(blc.getItem).value())
+                Bacon.zipAsArray(_(data).keys().map(blf.getItem).value())
                     .map(expect)
                     .assign('.toEqual', values);
             })
@@ -24,34 +32,26 @@ describe('bacon.localforage', function() {
     });
 
     it('should remove item', function(done) {
-        blc.removeItem('key3')
-            .flatMap(blc.getItem('key3'))
+        blf.removeItem('key3')
+            .flatMap(blf.getItem('key3'))
             .map(expect)
             .doAction('.toBeNull')
             .assign(done);
     });
 
     it('should manage key', function(done) {
-        blc.length()
+        blf.length()
             .map(expect)
             .doAction('.toBe', 3)
-            .flatMap(blc.keys())
+            .flatMap(blf.keys())
             .map('.sort')
             .doAction(function(keys) {
                 expect(keys).toEqual(_(data).keys().first(3).value());
-                Bacon.zipAsArray(_(3).range().map(blc.key).value())
+                Bacon.zipAsArray(_(3).range().map(blf.key).value())
                     .map('.sort')
                     .map(expect)
                     .assign('.toEqual', keys);
             })
-            .assign(done);
-    });
-
-    it('should clear storage', function(done) {
-        blc.clear()
-            .flatMap(blc.length())
-            .map(expect)
-            .doAction('.toBe', 0)
             .assign(done);
     });
 });
